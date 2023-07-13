0a2d326f2fc01ea4ee002442448d0c2f49fc9d26
Jorge Gabriel <jorge85x@gmail.com>
2023-07-13 18:36:51 -0300

    Adicionado get buscar por id de usuario e get buscar por todos

--- src/main/java/com/api/controledeestacionamento/controllers/ParkingSpotController.java
+++ src/main/java/com/api/controledeestacionamento/controllers/ParkingSpotController.java
@@ -2,11 +2,16 @@ package com.api.controledeestacionamento.controllers;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -49,4 +54,21 @@ public class ParkingSpotController {
     public ParkingSpotService getParkingSpotService() {
         return parkingSpotService;
     }
+    
+    @GetMapping //buscar por todos
+    public ResponseEntity<List<ParkingSpotModel>> getAllParkingSports(){
+    	return ResponseEntity.status(HttpStatus.OK).body(parkingSpotService.findAll());
+    }
+    
+    @GetMapping("/{id}") //buscar pelo id
+    public ResponseEntity<Object> getOneParkingSpot(@PathVariable(value = "id")UUID id){
+    	Optional<ParkingSpotModel> parkingSpotModelOptional = parkingSpotService.findById(id);
+    	if(!parkingSpotModelOptional.isPresent()) {
+    		return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Vaga não existe");
+    	}
+    	return ResponseEntity.status(HttpStatus.OK).body(parkingSpotModelOptional.get());
+    }
+    
+    
+    
 }
